56536012effbaed1c63d1f0a6eb42aa9a2a7e6e5
tripletime <tripletime@tlen.pl>
2015-06-17 03:36:51 +0200

    poprawnie działające do doszlifowania

--- js/myfhp.js
+++ js/myfhp.js
@@ -17,9 +17,9 @@ var pro = document.getElementById('probability');
 var probability = pro.value;
 var pause = true;
 var arr = [];
-var displayArr = [];
 var tab = [];
 var displayTab = [];
+var i, j, k;
 
 var range = document.getElementById('myRange');
 var rangeS = document.getElementById('myRangeS');
@@ -27,16 +27,20 @@ var wallX = range.value;
 var wallY = rangeS.value;
 var aliv = 0;
 
+var randomFill = true;
+
 /*
  * basic math functions
  */
 
 function round(value, x) {
-    var x = (!x ? 2 : x);
+    "use strict";
+    x = (!x ? 2 : x);
     return Math.round(value * Math.pow(10, x)) / Math.pow(10, x);
 }
 
 function ob(value) {
+    "use strict";
 	return Math.round(value);
 }
 
@@ -44,19 +48,13 @@ function ob(value) {
  * copy tables functions
  */
 
-function set(x, y) {
-	for (var i = 0; i < newSizeX; i++) {
-		for (var j = 0; j < newSizeY; j++) {
-			x[i][j] = y[i][j];
-		}
-	}
-}
-
 function setTab(x, y) {
-	for (var i = 0; i < sizeX; i++) {
-		for (var j = 0; j < sizeY; j++) {
-            for (var k = 0; k < sizeZ; k++) {
+    "use strict";
+	for (i = 0; i < sizeX; i += 1) {
+		for (j = 0; j < sizeY; j += 1) {
+            for (k = 0; k < sizeZ; k += 1) {
                 x[i][j][k] = y[i][j][k];
+                y[i][j][k] = -1;
             }
 		}
 	}
@@ -67,25 +65,25 @@ function setTab(x, y) {
  */
 
 function arrayBuild() {
-	arr = new Array(newSizeX);
-	displayArr = new Array(newSizeX);
-	for (i = 0; i < newSizeX; i++) {
-		arr[i] = new Array(newSizeY);
-		displayArr[i] = new Array(newSizeY);
+    "use strict";
+	arr = [newSizeX];
+	for (i = 0; i < newSizeX; i += 1) {
+		arr[i] = [newSizeY];
 	}
 }
 arrayBuild();
 
 function tableBuild() {
-    tab = new Array(sizeX);
-    displayTab = new Array(sizeX);
-    for (i = 0; i < sizeX; i++) {
-        tab[i] = new Array(sizeY);
-        displayTab[i] = new Array(sizeY);
-        for (j = 0; j < sizeY; j++) {
-            tab[i][j] = new Array(sizeZ);
-            displayTab[i][j] = new Array(sizeZ);
-            for (k = 0; k < sizeZ; k++) {
+    "use strict";
+    tab = [sizeX];
+    displayTab = [sizeX];
+    for (i = 0; i < sizeX; i += 1) {
+        tab[i] = [sizeY];
+        displayTab[i] = [sizeY];
+        for (j = 0; j < sizeY; j += 1) {
+            tab[i][j] = [sizeZ];
+            displayTab[i][j] = [sizeZ];
+            for (k = 0; k < sizeZ; k += 1) {
                 tab[i][j][k] = -1;
                 displayTab[i][j][k] = -1;
             }
@@ -99,11 +97,13 @@ tableBuild();
  */
 
 function pauseGame() {
+    "use strict";
 	pause = true;
 }
 
 function startGame() {
-	if (pause == true) {
+    "use strict";
+	if (pause === true) {
 		pro.disabled = true;
         range.disabled = true;
         rangeS.disabled = true;
@@ -113,7 +113,9 @@ function startGame() {
 }
 
 function restartGame() {
-	window.location.href = window.location.href;
+    "use strict";
+    var lock = window.location.href;
+	window.location.href = lock;
 }
 
 /*
@@ -121,20 +123,20 @@ function restartGame() {
  */
 
 function setProb(value) {
+    "use strict";
 	if (value >= 0 && value <= 100) {
 		probability = round(value, 2);
-	}
-	else {
+	} else {
 		pro.value = 10.00;
 		probability = 10.00;
 		alert("Wprowadź liczbę od 0 do 100");
 	}
 	randomFillGame();
-	//showAlive(probability);
 	firstStep();
 }
 
 function showAlive(value) {
+    "use strict";
 	var ali = document.getElementById('alive');
     var aliM = document.getElementById('aliveM');
     var aliright = document.getElementById('aliveright');
@@ -146,12 +148,14 @@ function showAlive(value) {
 }
 
 function showWall(value) {
+    "use strict";
     wallX = value;
     randomFillGame();
     firstStep();
 }
 
 function showWallS(value) {
+    "use strict";
     wallY = value;
     randomFillGame();
     firstStep();
@@ -162,19 +166,17 @@ function showWallS(value) {
  */
 
 function randomFillGame() {
+    "use strict";
     triangles();
     frames();
     wall(wallX, wallY);
-    manualFillGame();
+    if (randomFill === false) {
+        manualFillGame();
+    }
     makeTab();
     makeArr();
 }
 randomFillGame();
-
-function manualFillGame() {
-    arr[15][18] = 5;
-    arr[30][36] = 3;
-}
 
 /*
  * basic drawing
@@ -194,7 +196,6 @@ function triangles() {
             }
         }
     }
-    set(displayArr, arr);
 }
 
 function frames() {
@@ -205,21 +206,23 @@ function frames() {
             }
         }
     }
-    set(displayArr, arr);
 }
 
 function wall(value, values) {
 	for (var i = 1; i <= value; i++) {
 		for (var j = 1; j < newSizeY - 1; j++) {
 			if (i != Math.round(value)) {
-//				if (Math.abs(100 * Math.random()) < probability && (arr[i][j] == -1 || arr[i][j] == 1)) {
-//					arr[i][j] = Math.round(Math.random() * 5) + 1 ; // czerwony o kierunkach różnych 
-//				}
-//				else {
-//					continue;
-//				}
+                if (randomFill === true) {
+                    if (Math.abs(100 * Math.random()) < probability && (arr[i][j] == -1 || arr[i][j] == 1)) {
+                        arr[i][j] = Math.round(Math.random() * 5) + 1 ; // czerwony o kierunkach różnych 
+                    }
+                    else {
+                        continue;
+                    }
+                }
 			}
-			else if (j < newSizeX / 2 + Math.round(values) && j > newSizeX/ 2 - Math.round(values)) {
+			else if (j < newSizeX / 2 + Math.round(values) && j > newSizeX / 2 - Math.round(values)) {
+                // tu odjac jakby mialy byc rowne przegrody od dolu i gory
 				continue;
 			}
 			else {
@@ -227,7 +230,6 @@ function wall(value, values) {
 			}
 		}
 	}
-    set(displayArr, arr);
 }
 
 /*
@@ -260,7 +262,6 @@ function makeTab() {
             }            
         }
     }
-    setTab(displayTab, tab);
 }
 
 /*
@@ -297,7 +298,6 @@ function makeArr() {
     }
     //frames();
     //wall(wallX, wallY);
-    set(displayArr, arr);
 }
 
 /*
@@ -313,7 +313,7 @@ function display(value) {
 				value++;
 			}
             else if (arr[i][j] == -1) {
-				context.fillStyle = 'gray';
+				context.fillStyle = 'yellow';
 				context.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
 			}
 			else if (arr[i][j] == 0) {
@@ -339,73 +339,273 @@ function firstStep() {
 }
 firstStep();
 
+function manualFillGame() {
+    
+//    // narozniki OK
+//    arr[1][2] = 1;
+//    arr[93][2] = 2;
+//    arr[94][4] = 2;
+//    arr[2][92] = 5;
+//    arr[1][90] = 5;
+//    arr[94][92] = 4;
+//
+//    // narozniki sciany OK
+//    arr[31][2] = 2;
+//    arr[35][2] = 1;
+//    arr[32][4] = 2;
+//    arr[34][4] = 1;
+//    
+//    // random lewa komora OK
+//    arr[15][18] = 5;
+//    
+//    // sciana OK
+//    arr[8][4] = 3;
+//    arr[9][6] = 3;
+//    arr[10][8] = 3;
+//    
+//    // szczelina OK
+//    arr[28][40] = 3;
+//    arr[29][42] = 3;
+//    arr[28][44] = 3;
+//    arr[29][46] = 3;
+//    arr[28][52] = 3;
+//    arr[29][54] = 3;
+//    
+//    // ukosy przy szczelinie OK
+//    arr[30][36] = 4;
+//    arr[30][40] = 4;
+//    arr[31][38] = 4;
+//    arr[31][42] = 4;
+    
+    // test zderzen czastek ZLE NIE DZIALAJA KOLIZJE DWOCH CZASTECZEK
+    arr[8][4] = 3;
+    arr[9][6] = 3;
+    arr[10][8] = 3;
+    arr[14][4] = 6;
+    arr[13][6] = 6;
+    arr[12][8] = 6;
+}
+
+
 function step() {
-    // glowna funkcjonalnosc kroku wraz z kolizjami
+    
+    // glowna funkcjonalnosc kolizji dwoch czasteczek
+    for (var i = 0; i < sizeX; i++) {
+        for (var j = 0; j < sizeY; j++) {
+            if (tab[i][j][1] == 1 && tab[i][j][4] == 4) {
+                tab[i][j][1] = -1;
+                tab[i][j][4] = -1;
+                if (Math.abs(100 * Math.random()) < 50) {
+                    tab[i][j][2] = 2;
+                    tab[i][j][5] = 5;
+                }
+                else {
+                    tab[i][j][3] = 3;
+                    tab[i][j][6] = 6;
+                }
+            }
+            else if (tab[i][j][2] == 2 && tab[i][j][5] == 5) {
+                tab[i][j][2] = -1;
+                tab[i][j][5] = -1;
+                if (Math.abs(100 * Math.random()) < 50) {
+                    tab[i][j][1] = 1;
+                    tab[i][j][4] = 4;
+                }
+                else {
+                    tab[i][j][3] = 3;
+                    tab[i][j][6] = 6;
+                }
+            }
+            else if (tab[i][j][3] == 3 && tab[i][j][6] == 6) {
+                tab[i][j][3] = -1;
+                tab[i][j][6] = -1;
+                if (Math.abs(100 * Math.random()) < 50) {
+                    tab[i][j][2] = 2;
+                    tab[i][j][5] = 5;
+                }
+                else {
+                    tab[i][j][1] = 1;
+                    tab[i][j][4] = 4;
+                }
+            }
+    
+        }
+    }
+    
+    // glowna funkcjonalnosc zmiany kierunków
     for (var i = 0; i < sizeX; i++) {
         for (var j = 0; j < sizeY; j++) {
             // obramowanie tabeli
             if (i == 0 || j == 0 || i == sizeX - 1 || j == sizeY - 1) {
+                // narozniki
+                // *\
+                if (i == 0 && j == 0 && tab[i][j][1] == 1) {
+                    tab[i][j][1] = -1;
+                    tab[i][j][4] = 4;
+                }
+                // */
+                else if (i == 0 && j == sizeY - 1) {
+                    if (tab[i][j][5] == 5) {
+                        tab[i][j][5] = -1;
+                        tab[i][j][4] = 4;
+                    }
+                    if (tab[i][j][4] == 4) {
+                        tab[i][j][4] = -1;
+                        tab[i][j][2] = 2;
+                    }
+                }
+                // /*
+                else if (i == sizeX - 1 && j == 0) {
+                    if (tab[i][j][2] == 2) {
+                        tab[i][j][2] = -1;
+                        tab[i][j][4] = 4;
+                    }
+                    if (tab[i][j][1] == 1) {
+                        tab[i][j][1] = -1;
+                        tab[i][j][5] = 5;
+                    }
+                }
+                // \*
+                else if (i == sizeX - 1 && j == sizeY - 1 && tab[i][j][4] == 4) {
+                    tab[i][j][4] = -1;
+                    tab[i][j][1] = 1;
+                }
+                // rogi tworzone przez przegrode
+                // dol
+                else if (i == ((wallX - 1) / 2) && j == sizeY - 1) {
+                    if (tab[i][j][5] == 5) {
+                        tab[i][j][5] = -1;
+                        tab[i][j][2] = 2;
+                    }
+                    if (tab[i][j][4] == 4) {
+                        tab[i][j][4] = -1;
+                        tab[i][j][1] = 1;
+                    }
+                }
+                // sciany
                 // lewa sciana
-                if (i == 0) {
-                    if (displayTab[i][j][6] == 6) {
-                        displayTab[i][j][6] = -1;
-                        displayTab[i][j][3] = 3;
+                else if (i == 0) {
+                    if (tab[i][j][6] == 6) {
+                        tab[i][j][6] = -1;
+                        tab[i][j][3] = 3;
                     }
                     // wiersze parzyste 0, 2 itd.
                     if (j % 2 == 0) {
-                        if (displayTab[i][j][1] == 1) {
-                            displayTab[i][j][1] = -1;
-                            displayTab[i][j][2] = 2;
+                        if (tab[i][j][1] == 1) {
+                            tab[i][j][1] = -1;
+                            tab[i][j][2] = 2;
                         }
-                        if (displayTab[i][j][5] == 5) {
-                            displayTab[i][j][5] = -1;
-                            displayTab[i][j][4] = 4;
+                        if (tab[i][j][5] == 5) {
+                            tab[i][j][5] = -1;
+                            tab[i][j][4] = 4;
                         }
                     }
                 }
                 // prawa sciana
                 else if (i == sizeX - 1) {
-                    if (displayTab[i][j][3] == 3) {
-                        displayTab[i][j][3] = -1;
-                        displayTab[i][j][6] = 6;
+                    if (tab[i][j][3] == 3) {
+                        tab[i][j][3] = -1;
+                        tab[i][j][6] = 6;
                     }
                     // wiersze nieparzyste 1, 3 itd.
                     if (j % 2 == 1) {
-                        if (displayTab[i][j][2] == 2) {
-                            displayTab[i][j][2] = -1;
-                            displayTab[i][j][1] = 1;
+                        if (tab[i][j][2] == 2) {
+                            tab[i][j][2] = -1;
+                            tab[i][j][1] = 1;
                         }
-                        if (displayTab[i][j][4] == 4) {
-                            displayTab[i][j][4] = -1;
-                            displayTab[i][j][5] = 5;
+                        if (tab[i][j][4] == 4) {
+                            tab[i][j][4] = -1;
+                            tab[i][j][5] = 5;
                         }
                     }
                 }
                 // gorna sciana
                 else if (j == 0) {
-                    if (displayTab[i][j][1] == 1) {
-                        displayTab[i][j][1] == -1;
-                        displayTab[i][j][5] == 5; 
+                    if (tab[i][j][1] == 1) {
+                        tab[i][j][1] = -1;
+                        tab[i][j][5] = 5; 
                     }
-                    if (displayTab[i][j][2] == 2) {
-                        displayTab[i][j][2] == -1;
-                        displayTab[i][j][4] == 4; 
+                    if (tab[i][j][2] == 2) {
+                        tab[i][j][2] = -1;
+                        tab[i][j][4] = 4; 
                     }
                 }
                 // dolna sciana
                 else if (j == sizeY - 1) {
-                    if (displayTab[i][j][5] == 5) {
-                        displayTab[i][j][5] == -1;
-                        displayTab[i][j][1] == 1; 
+                    if (tab[i][j][5] == 5) {
+                        tab[i][j][5] = -1;
+                        tab[i][j][1] = 1; 
                     }
-                    if (displayTab[i][j][4] == 4) {
-                        displayTab[i][j][4] == -1;
-                        displayTab[i][j][2] == 2; 
+                    if (tab[i][j][4] == 4) {
+                        tab[i][j][4] = -1;
+                        tab[i][j][2] = 2; 
                     }
                 }
             }
+            // przegroda
+            else if (j > (sizeX / 2 + Math.round(wallY / 2 - 1)) || j < (sizeX / 2 - Math.round(wallY / 2 - 1))) {
+                if (i == (wallX - 1) / 2) {
+                    if (j % 2 == 0) {
+                        if (tab[i][j][3] == 3) {
+                            tab[i][j][3] = -1;
+                            tab[i][j][6] = 6;
+                        }
+                    }
+                    else if (j % 2 == 1) {
+                        if (tab[i][j][5] == 5 && tab[i][j][4] == 4) {
+                            
+                        }
+                        else if (tab[i][j][5] == 5) {
+                            tab[i][j][5] = -1;
+                            tab[i][j][4] = 4;
+                        }
+                        else if (tab[i][j][4] == 4) {
+                            tab[i][j][4] = -1;
+                            tab[i][j][5] = 5;
+                        }
+                        if (tab[i][j][1] == 1 && tab[i][j][2] == 2) {
+                            
+                        }
+                        else if (tab[i][j][1] == 1) {
+                            tab[i][j][1] = -1;
+                            tab[i][j][2] = 2;
+                        }
+                        else if (tab[i][j][2] == 2) {
+                            tab[i][j][2] = -1;
+                            tab[i][j][1] = 1;
+                        }
+                        if (tab[i][j][3] == 3 && tab[i][j][6] == 6) {
+                            
+                        }
+                        else if (tab[i][j][3] == 3) {
+                            tab[i][j][3] = -1;
+                            tab[i][j][6] = 6;
+                        }
+                        else if (tab[i][j][6] == 6) {
+                            tab[i][j][6] = -1;
+                            tab[i][j][3] = 3;
+                        }
+                    }
+                }
+                // przegroda od lewej żeby nie przechodziło za zielone
+                else if (i == ((wallX - 1) / 2) + 1) {
+                    if (j % 2 == 0) {
+                        if (tab[i][j][6] == 6) {
+                            tab[i][j][6] = -1;
+                            tab[i][j][3] = 3;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    // glowna funkcjonalnosc przepisywania tablicy
+    for (var i = 0; i < sizeX; i++) {
+        for (var j = 0; j < sizeY; j++) {
             for (var k = 0; k < sizeZ; k++) {
                 current_value = tab[i][j][k];
+                
                 // wiersze parzyste 0, 2 itd.
                 if (j % 2 == 0 && current_value >= 1 && current_value <= 6) {
                     switch(k) {
@@ -428,10 +628,9 @@ function step() {
                             displayTab[i-1][j][k] = current_value;
                             break;
                     }
-                    displayTab[i][j][k] = -1;        
                 }
                 // wiersze nieparzyste 1, 3 itd.
-                if (j % 2 == 1 && current_value >= 1 && current_value <= 6) {
+                else if (j % 2 == 1 && current_value >= 1 && current_value <= 6) {
                     switch(k) {
                         case 1:
                             displayTab[i][j-1][k] = current_value;
@@ -452,17 +651,16 @@ function step() {
                             displayTab[i-1][j][k] = current_value;
                             break;
                     }
-                    displayTab[i][j][k] = -1;        
                 }
             }
         }
     }
     
+    // zamiana i czyszczenie tablicy
     setTab(tab, displayTab);
     
     // przepisanie do arr wyswietlonych
     makeArr();
-    set(arr, displayArr);
 
     // funkcja liczaca slupki procentowe po arr wyswietlonych
     aliv = 0;
